96eb8cf02461feed35a0dc2fd6b05dfa9c5238f4
Suhail Razzak <suhail.razzak@gmail.com>
2025-07-28 15:53:17 -0400

    update main file

--- src/mdx.rs
+++ src/mdx.rs
@@ -1,9 +1,7 @@
-use std::{fs, collections::HashSet, env};
+use std::{fs, env};
 use zed_extension_api::{self as zed, settings::LspSettings};
 
-struct MDXExtension {
-    installed: HashSet<String>,
-}
+struct MDXExtension;
 
 const PACKAGE_NAME: &str = "@mdx-js/language-server";
 const SERVER_PATH: &str = "node_modules/.bin/mdx-language-server";
@@ -43,11 +41,8 @@ impl MDXExtension {
 
 impl zed::Extension for MDXExtension {
     fn new() -> Self {
-        Self {
-            installed: HashSet::new(),
-        }
+        Self
     }
-
 
     fn language_server_initialization_options(
         &mut self,
@@ -109,25 +104,3 @@ impl zed::Extension for MDXExtension {
 }
 
 zed::register_extension!(MDXExtension);
-
-/// Extensions to the Zed extension API that have not yet stabilized.
-mod zed_ext {
-    /// Sanitizes the given path to remove the leading `/` on Windows.
-    ///
-    /// On macOS and Linux this is a no-op.
-    ///
-    /// This is a workaround for https://github.com/bytecodealliance/wasmtime/issues/10415.
-    pub fn sanitize_windows_path(path: std::path::PathBuf) -> std::path::PathBuf {
-        use zed_extension_api::{current_platform, Os};
-
-        let (os, _arch) = current_platform();
-        match os {
-            Os::Mac | Os::Linux => path,
-            Os::Windows => path
-                .to_string_lossy()
-                .to_string()
-                .trim_start_matches('/')
-                .into(),
-        }
-    }
-}
